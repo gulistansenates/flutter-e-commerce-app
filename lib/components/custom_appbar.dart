9dbd12d3f6f3b563b24160373f16335634371bed
gulistansenates <gulistansenates@gmail.com>
2025-05-11 01:00:28 +0300

    fix appbar

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:e_commerce_app/constant/constant.dart';
 import 'package:grock/grock.dart';
 import 'package:e_commerce_app/assets.dart';
-import 'package:e_commerce_app/constant/constant.dart';
 
 class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   final String appbarTitle;
@@ -15,6 +15,7 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      centerTitle: true,
       title: Text(
         appbarTitle,
         style: const TextStyle(
@@ -24,16 +25,12 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
       ),
       leading: Padding(
         padding: 12.allP,
-        child: SvgPicture.asset(
-          Assets.icons.ic_drawer_svg, 
-        ),
+        child: SvgPicture.asset(Assets.icons.ic_drawer_svg),
       ),
       actions: [
         Padding(
           padding: 12.allP,
-          child: SvgPicture.asset(
-            Assets.icons.ic_search_svg, 
-          ),
+          child: SvgPicture.asset(Assets.icons.ic_search_svg),
         ),
       ],
       backgroundColor: Constant.darkWhite,
